20bdf809b1be83065af859799c063f5669c03ef8
sina <kadkhodaeisina@gmail.com>
2025-10-12 00:33:03 +0330

    test(server): Apply user balance

--- tests/server/user_balance_test.go
+++ tests/server/user_balance_test.go
@@ -28,3 +28,23 @@ func Test_UserBalance_BadData(t *testing.T) {
 
 	require.ErrorIs(t, tx.Error, gorm.ErrRecordNotFound)
 }
+
+func Test_UserBalance_Success(t *testing.T) {
+	jsonBody := `{
+		"email": "test@example.com",
+		"balance": 100000
+	}`
+
+	req := httptest.NewRequest(fiber.MethodPost, "/api/user/balance", bytes.NewReader([]byte(jsonBody)))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, _ := app.Test(req)
+	require.Equal(t, resp.StatusCode, fiber.StatusOK)
+
+	var user models.User
+	tx := database.Instance().First(&user, "email = ?", "test@example.com")
+
+	require.NotErrorIs(t, tx.Error, gorm.ErrRecordNotFound)
+	require.NotEqual(t, user.ID.String(), "00000000-0000-0000-0000-000000000000")
+	require.Equal(t, user.Balance, float32(100000))
+}
