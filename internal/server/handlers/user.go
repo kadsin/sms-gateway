ba723ced1eeae9e32b02093c3ff87e80342c2486
sina <kadkhodaeisina@gmail.com>
2025-10-12 00:33:52 +0330

    feat(server): Create or update a user balance

--- internal/server/handlers/user.go
+++ internal/server/handlers/user.go
@@ -2,14 +2,20 @@ package handlers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/kadsin/sms-gateway/database"
+	"github.com/kadsin/sms-gateway/database/models"
 	"github.com/kadsin/sms-gateway/internal/server/requests"
 )
 
 func ChangeUserBalance(c *fiber.Ctx) error {
-	_, err := requests.Prepare[requests.UserBalanceRequest](c)
+	data, err := requests.Prepare[requests.UserBalanceRequest](c)
 	if err != nil {
 		return fiber.NewError(fiber.StatusUnprocessableEntity, err.Error())
 	}
 
-	return nil
+	tx := database.Instance().Where("email", data.Email).
+		Assign("balance", data.Balance).
+		FirstOrCreate(&models.User{})
+
+	return tx.Error
 }
