37b2ab66455727c81d4e531967a6c7e794ac6a46
sina <kadkhodaeisina@gmail.com>
2025-10-14 22:47:30 +0330

    test(server): Calculate sms price

--- tests/server/send_sms_test.go
+++ tests/server/send_sms_test.go
@@ -11,6 +11,8 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/kadsin/sms-gateway/config"
 	"github.com/kadsin/sms-gateway/internal/container"
+	"github.com/kadsin/sms-gateway/internal/dtos"
+	"github.com/kadsin/sms-gateway/internal/dtos/messages"
 	"github.com/kadsin/sms-gateway/tests/mocks"
 	"github.com/stretchr/testify/require"
 )
@@ -42,4 +44,32 @@ func Test_SendSms_BadData(t *testing.T) {
 	c.(*mocks.KafkaConsumerMock).Topic = config.Env.Kafka.Topics.Express
 	_, err = c.FetchMessage(context.Background())
 	require.NotNil(t, err)
+}
+
+func Test_SendSms_CalculatePrice(t *testing.T) {
+	user := createUser()
+
+	jsonBody := fmt.Sprintf(`{
+        "client_email": "%s",
+        "receiver_phone": "+989123456789",
+        "content": "abcd",
+        "is_express": false
+	}`, user.Email)
+
+	req := httptest.NewRequest(fiber.MethodPost, "/api/sms", bytes.NewReader([]byte(jsonBody)))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, _ := app.Test(req)
+	require.Equal(t, resp.StatusCode, fiber.StatusOK)
+
+	c := container.KafkaConsumer("")
+	c.(*mocks.KafkaConsumerMock).Topic = config.Env.Kafka.Topics.Regular
+	m, _ := c.FetchMessage(context.Background())
+
+	sms, _ := dtos.Unmarshal[messages.Sms](m.Value)
+	require.Equal(t, 40, sms.Price)
+
+	oldBalance := user.Balance
+	container.DB().Find(&user, "id", user.ID)
+	require.Equal(t, oldBalance-40, user.Balance)
 }
