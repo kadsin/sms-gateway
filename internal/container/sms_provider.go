59166b4b586171b3f4d70975726449dd13bef5d4
sina <kadkhodaeisina@gmail.com>
2025-10-16 00:04:09 +0330

    feat: Add fake sms provider

---
+++ internal/container/sms_provider.go
@@ -0,0 +1,27 @@
+package container
+
+import (
+	"fmt"
+	"math/rand/v2"
+
+	"github.com/kadsin/sms-gateway/internal/dtos/messages"
+	"github.com/kadsin/sms-gateway/internal/sms"
+)
+
+type fakeSmsProvider struct{}
+
+func (fakeSmsProvider) Send(m messages.Sms) error {
+	if sent := rand.IntN(2) == 1; !sent {
+		return fmt.Errorf("error on sending message with id %v", m.Id)
+	}
+
+	return nil
+}
+
+func SmsProvider() sms.Provider {
+	if mockSmsProviderInstance != nil {
+		return mockSmsProviderInstance
+	}
+
+	return &fakeSmsProvider{}
+}
