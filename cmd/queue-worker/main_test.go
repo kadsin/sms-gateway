7a59e48f142d6bd628583abcb09bf7e65c00360a
sina <kadkhodaeisina@gmail.com>
2025-10-16 20:05:38 +0330

    perf(queue worker): Use userbalance to update balance

--- cmd/queue-worker/main_test.go
+++ cmd/queue-worker/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"testing"
 
@@ -8,6 +9,7 @@ import (
 	analytics_models "github.com/kadsin/sms-gateway/analytics/models"
 	"github.com/kadsin/sms-gateway/internal/container"
 	"github.com/kadsin/sms-gateway/internal/dtos/messages"
+	userbalance "github.com/kadsin/sms-gateway/internal/user_balance"
 	"github.com/kadsin/sms-gateway/tests"
 	"github.com/stretchr/testify/require"
 )
@@ -29,27 +31,27 @@ func TestMain(m *testing.M) {
 
 func Test_Queue_SendSms(t *testing.T) {
 	m1 := tests.CreateSmsMessage(uuid.MustParse("ad760a00-3a02-4678-94cb-3546891dc083"))
-	sendSms(&mockProvider{fail: true}, m1)
+	sendSms(context.Background(), &mockProvider{fail: true}, m1)
 
 	var sms1 analytics_models.SmsMessage
 	container.Analytics().Model(&analytics_models.SmsMessage{}).Find(&sms1, "id", m1.Id)
 	require.Equal(t, sms1.Status, analytics_models.SMS_FAILED)
 
 	m2 := tests.CreateSmsMessage(uuid.MustParse("ad760a00-3a02-4678-94cb-3546891dc083"))
-	sendSms(&mockProvider{fail: false}, m2)
+	sendSms(context.Background(), &mockProvider{fail: false}, m2)
 
 	var sms2 analytics_models.SmsMessage
 	container.Analytics().Model(&analytics_models.SmsMessage{}).Find(&sms2, "id", m2.Id)
 	require.Equal(t, sms2.Status, analytics_models.SMS_SENT)
 }
 
-func Test_Queue_IncreaseBalanceOnFailure(t *testing.T) {
+func Test_Queue_RefundOnFailure(t *testing.T) {
 	user := tests.CreateUser()
+	oldBalance, _ := userbalance.Get(context.Background(), user.ID)
 
 	m := tests.CreateSmsMessage(user.ID)
-	sendSms(&mockProvider{fail: true}, m)
+	sendSms(context.Background(), &mockProvider{fail: true}, m)
 
-	oldBalance := user.Balance
-	container.DB().Find(&user, "id", user.ID)
-	require.Equal(t, oldBalance+m.Price, user.Balance)
+	newBalance, _ := userbalance.Get(context.Background(), user.ID)
+	require.Equal(t, oldBalance+m.Price, newBalance)
 }
